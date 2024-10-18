f9a0585a3ed40a88909dcf0bfaeedafb36f5565d
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-18 20:44:29 +0300

    chore: code config

---
+++ packages/styles/stylelint.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        "stylelint-stylus/recommended",
+        "stylelint-stylus/standard",
+    ],
+    rules: {
+        "stylus/declaration-colon": "never"
+    },
+};
