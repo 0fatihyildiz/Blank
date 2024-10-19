ad96263586a53b7bb51ba1e4b0869db69eca3efa
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-19 05:06:55 +0300

    Feat/icon (#6)
    * feat: react remix icon component
    
    * feat: vue icon component

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@blank/react'
+import { Button, Icon } from '@blank/react'
 
 function App() {
 	return (
 		<div>
+			<Icon name="RiAppStoreLine" size={24} color="black" />
 			<Button />
 		</div>
 	)
