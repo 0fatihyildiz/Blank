c44085fbca7bc8caee77a3a2298cc4446f08e1d8
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-20 16:44:41 +0300

    refactor: some stylus codes

--- playground/vue/src/index.vue
+++ playground/vue/src/index.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-import { Button, Container } from '@blank/vue'
+import { Button, Container, Icon } from '@blank/vue'
 </script>
 
 <template>
 	<Container>
-		<Button>Click me</Button>
-		<Button variant="secondary">
+		<Button>
 			Click me
 		</Button>
+
+		<Button variant="secondary">
+			<Icon name="RiCommandFill" size="24" />
+			Click me
+		</Button>
+
 		<Button variant="tertiary">
 			Click me
 		</Button>
+
 		<Button variant="quaternary">
 			Click me
 		</Button>
+
 		<Button variant="destructive">
 			Click me
 		</Button>
