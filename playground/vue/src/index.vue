813488b33cd8ee267446e78ba0b3ec0b763dacd8
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-20 17:06:00 +0300

    refactor: button styles changed

--- playground/vue/src/index.vue
+++ playground/vue/src/index.vue
@@ -5,24 +5,33 @@ import { Button, Container, Icon } from '@blank/vue'
 <template>
 	<Container>
 		<Button>
+			<Icon name="RiCommandFill" size="24" />
 			Click me
+			<Icon name="RiArrowRightLine" size="24" />
 		</Button>
 
 		<Button variant="secondary">
 			<Icon name="RiCommandFill" size="24" />
 			Click me
+			<Icon name="RiArrowRightLine" size="24" />
 		</Button>
 
 		<Button variant="tertiary">
+			<Icon name="RiCommandFill" size="24" />
 			Click me
+			<Icon name="RiArrowRightLine" size="24" />
 		</Button>
 
 		<Button variant="quaternary">
+			<Icon name="RiCommandFill" size="24" />
 			Click me
+			<Icon name="RiArrowRightLine" size="24" />
 		</Button>
 
 		<Button variant="destructive">
-			Click me
+			<Icon name="RiCommandFill" size="24" />
+			Don't Click me
+			<Icon name="RiArrowRightLine" size="24" />
 		</Button>
 	</Container>
 </template>
