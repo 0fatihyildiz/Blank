8d77b4d36fc6b3337845b91656e21b7020bc54dd
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-20 18:22:04 +0300

    Feat/company logos (#9)
    * feat: create logos
    
    * feat: a new communication in project name of shared
    
    * refactor: shared icons compiler change to tsup
    
    ---------
    
    Co-authored-by: Ferhat-kus <55ferhat.kus@gmail.com>

---
+++ shared/types/src/components/button.d.ts
@@ -0,0 +1,6 @@
+interface ButtonProps {
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'quaternary' | 'destructive';
+    size?: 32 | 40 | 48;
+    disabled?: boolean;
+}
+export type { ButtonProps };
---
+++ shared/types/src/components/button.ts
@@ -0,0 +1,7 @@
+interface ButtonProps {
+	variant?: 'primary' | 'secondary' | 'tertiary' | 'quaternary' | 'destructive'
+	size?: 32 | 40 | 48
+	disabled?: boolean
+}
+
+export type { ButtonProps }
