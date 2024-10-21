61ea16cf618c078d6f1fe8943cfdeac225fc3677
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-21 21:39:35 +0300

    Feat/react/button (#11)
    * feat: sharde types done
    
    * chore: github action pnpm verison
    
    * chore: husky require dbuild
    
    * chore: workflows status changed draft
    
    * feat: react button component

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -1,9 +1,37 @@
-import { Button } from '@blank/react'
+import { Button, Icon } from '@blank/react'
 
 function App() {
 	return (
 		<div>
-			<Button />
+			<Button size="small">
+				<Icon name="RiCommandFill" size={20} />
+				Click me
+				<Icon name="RiArrowRightLine" size={20} />
+			</Button>
+
+			<Button size="small" variant="secondary">
+				<Icon name="RiCommandFill" size={20} />
+				Click me
+				<Icon name="RiArrowRightLine" size={20} />
+			</Button>
+
+			<Button size="small" variant="tertiary">
+				<Icon name="RiCommandFill" size={20} />
+				Click me
+				<Icon name="RiArrowRightLine" size={20} />
+			</Button>
+
+			<Button size="small" variant="quaternary">
+				<Icon name="RiCommandFill" size={20} />
+				Click me
+				<Icon name="RiArrowRightLine" size={20} />
+			</Button>
+
+			<Button size="small" variant="destructive">
+				<Icon name="RiCommandFill" size={20} />
+				Don't Click me
+				<Icon name="RiArrowRightLine" size={20} />
+			</Button>
 		</div>
 	)
 }
