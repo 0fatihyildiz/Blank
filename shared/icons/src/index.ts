0c11a827d840df3399b2f0c0f32859f56777d7c4
Ferhat-kus <55ferhat.kus@gmail.com>
2024-10-24 12:33:19 +0300

    feat:all icons added

--- shared/icons/src/index.ts
+++ shared/icons/src/index.ts
@@ -1,2 +1,14 @@
-import Logos from "./icons/logos.json";
-export { Logos };
+import CompanyLogos from "./icons/company-logos.json";
+import Crypto from "./icons/crypto-logos.json";
+import Stars from "./icons/stars.json";
+import Status from "./icons/status.json";
+import PaymentsMethodsLogos from "./icons/payments-methods-logos.json";
+import SocialMedia from "./icons/social-media.json";
+export {
+    CompanyLogos,
+    Crypto,
+    Stars,
+    Status,
+    PaymentsMethodsLogos,
+    SocialMedia,
+};
