28138e089ecd0a1b1084d6142083e932911986ac
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-10-27 00:05:31 +0300

    fix: eslint fix

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -1,39 +1,39 @@
 import { Button, Icon } from '@blank/react'
 
 function App() {
-	return (
-		<div>
-			<Button size="small">
-				<Icon name="RiCommandFill" size={20} />
-				Click me
-				<Icon name="RiArrowRightLine" size={20} />
-			</Button>
+    return (
+        <div>
+            <Button size="small">
+                <Icon name="RiCommandFill" size={20} />
+                Click me
+                <Icon name="RiArrowRightLine" size={20} />
+            </Button>
 
-			<Button size="small" variant="secondary">
-				<Icon name="RiCommandFill" size={20} />
-				Click me
-				<Icon name="RiArrowRightLine" size={20} />
-			</Button>
+            <Button size="small" variant="secondary">
+                <Icon name="RiCommandFill" size={20} />
+                Click me
+                <Icon name="RiArrowRightLine" size={20} />
+            </Button>
 
-			<Button size="small" variant="tertiary">
-				<Icon name="RiCommandFill" size={20} />
-				Click me
-				<Icon name="RiArrowRightLine" size={20} />
-			</Button>
+            <Button size="small" variant="tertiary">
+                <Icon name="RiCommandFill" size={20} />
+                Click me
+                <Icon name="RiArrowRightLine" size={20} />
+            </Button>
 
-			<Button size="small" variant="quaternary">
-				<Icon name="RiCommandFill" size={20} />
-				Click me
-				<Icon name="RiArrowRightLine" size={20} />
-			</Button>
+            <Button size="small" variant="quaternary">
+                <Icon name="RiCommandFill" size={20} />
+                Click me
+                <Icon name="RiArrowRightLine" size={20} />
+            </Button>
 
-			<Button size="small" variant="destructive">
-				<Icon name="RiCommandFill" size={20} />
-				Don't Click me
-				<Icon name="RiArrowRightLine" size={20} />
-			</Button>
-		</div>
-	)
+            <Button size="small" variant="destructive">
+                <Icon name="RiCommandFill" size={20} />
+                Don't Click me
+                <Icon name="RiArrowRightLine" size={20} />
+            </Button>
+        </div>
+    )
 }
 
 export default App
