1cb4af6bf0247f5b49b75fa9a4db72d325aa5973
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-11-01 21:48:38 +0300

    feat: button group closes #16 (#26)
    * feat: button group #16
    
    * chore(type): size set optional

--- packages/vue/core/components/button.vue
+++ packages/vue/core/components/button.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { ButtonProps } from '@blank/types'
-import { computed } from 'vue'
+import { computed, inject, onMounted, ref } from 'vue'
 import Icon from '../common/icon.vue'
 
 const props = withDefaults(defineProps<ButtonProps>(), {
@@ -10,17 +10,31 @@ const props = withDefaults(defineProps<ButtonProps>(), {
 	disabled: false,
 })
 
-const classes = computed(() => {
-	return {
-		[props.appearance]: true,
-		[props.variant]: true,
-		[props.size]: true,
+const variant = ref(props.variant)
+const size = ref(props.size)
+const appearance = ref(props.appearance)
+
+const buttonGroup = inject('buttonGroup') as {
+	size: 'small' | 'medium' | 'large'
+}
+
+onMounted(() => {
+	if (buttonGroup) {
+		size.value = buttonGroup.size || size.value
+		variant.value = 'secondary'
+		appearance.value = 'default'
 	}
 })
+
+const classes = computed(() => ({
+	[appearance.value]: true,
+	[variant.value]: true,
+	[size.value]: true,
+}))
 </script>
 
 <template>
-	<button class="blank__button" :class="classes" :disabled="disabled">
+	<button class="blank__button" :class="classes" :disabled="props.disabled">
 		<slot />
 	</button>
 </template>
