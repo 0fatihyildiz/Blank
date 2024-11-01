3e6adb82561ab667c1dc770f6152e7845c874b42
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-11-01 23:14:56 +0300

    chore: linter added in stylus

--- packages/styles/stylelint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: [
-        'stylelint-stylus/recommended',
-        'stylelint-stylus/standard',
-    ],
-    rules: {
-        'stylus/declaration-colon': 'never',
-    },
-}
