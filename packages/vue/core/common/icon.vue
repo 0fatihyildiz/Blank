7e404021b6ba95f5df3838bff7ec79e642e5a040
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-11-01 23:40:46 +0300

    chore: linter fix

--- packages/vue/core/common/icon.vue
+++ packages/vue/core/common/icon.vue
@@ -4,13 +4,13 @@ import { Remix, Status } from '@blank/icons'
 import { computed, defineComponent } from 'vue'
 
 const props = withDefaults(defineProps<IconProps>(), {
-	size: '24',
-	color: 'currentColor',
+    size: '24',
+    color: 'currentColor',
 })
 
 const Icons: Record<string, Record<string, any>> = {
-	remix: Remix,
-	status: Status,
+    remix: Remix,
+    status: Status,
 }
 
 const [prefix, iconName] = props.name.split(':') as [keyof typeof Icons, string]
@@ -18,22 +18,22 @@ const [prefix, iconName] = props.name.split(':') as [keyof typeof Icons, string]
 const iconData = computed(() => Icons[prefix]?.icons[iconName])
 
 const IconNotFound = defineComponent({
-	setup() {
-		console.error(`Icon ${props.name} not found`)
-	},
-	template: '<!-- Icon not found -->',
+    setup() {
+        console.error(`Icon ${props.name} not found`)
+    },
+    template: '<!-- Icon not found -->',
 })
 </script>
 
 <template>
-	<svg
-		v-if="iconData"
-		:height="props.size"
-		:width="props.size"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		v-html="iconData.body"
-	/>
-	<IconNotFound v-else />
+    <svg
+        v-if="iconData"
+        :height="props.size"
+        :width="props.size"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        v-html="iconData.body"
+    />
+    <IconNotFound v-else />
 </template>
--- packages/vue/eslint.config.js
+++ packages/vue/eslint.config.js
@@ -2,8 +2,9 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
     stylistic: {
-        indent: 'tab',
+        indent: 4,
         quotes: 'single',
+        jsx: true,
         semi: false,
     },
     rules: {
