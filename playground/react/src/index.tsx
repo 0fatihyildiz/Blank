20d86a8e406e92b89540a840757e6fb47bd2d92c
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-11-02 18:22:05 +0300

    feat: logos and icons closes #17 (#30)
    * feat: logos and icons
    
    * fix: eslint

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -4,33 +4,33 @@ function App() {
 	return (
 		<div>
 			<Button size="small">
-				<Icon name="remix:command-fill" size={20} />
+				<Icon name="remix:command-fill" />
 				Click me
-				<Icon name="remix:arrow-right-line" size={20} />
+				<Icon name="remix:arrow-right-line" />
 			</Button>
 
 			<Button size="small" variant="secondary">
-				<Icon name="remix:command-fill" size={20} />
+				<Icon name="remix:command-fill" />
 				Click me
-				<Icon name="remix:arrow-right-line" size={20} />
+				<Icon name="remix:arrow-right-line" />
 			</Button>
 
 			<Button size="small" variant="tertiary">
-				<Icon name="remix:command-fill" size={20} />
+				<Icon name="remix:command-fill" />
 				Click me
-				<Icon name="remix:arrow-right-line" size={20} />
+				<Icon name="remix:arrow-right-line" />
 			</Button>
 
 			<Button size="small" variant="quaternary">
-				<Icon name="remix:command-fill" size={20} />
+				<Icon name="remix:command-fill" />
 				Click me
-				<Icon name="remix:arrow-right-line" size={20} />
+				<Icon name="remix:arrow-right-line" />
 			</Button>
 
 			<Button size="small" variant="destructive">
-				<Icon name="remix:command-fill" size={20} />
+				<Icon name="remix:command-fill" />
 				Don't Click me
-				<Icon name="remix:arrow-right-line" size={20} />
+				<Icon name="remix:arrow-right-line" />
 			</Button>
 		</div>
 	)
--- shared/icons/src/index.ts
+++ shared/icons/src/index.ts
@@ -1,2 +1,6 @@
-export * from './icons'
-export * from './logos'
+import Cryptocurrency from '../json/cryptocurrency.json'
+import Logos from '../json/logos.json'
+import Payment from '../json/payment.json'
+import Remix from '../json/remix.json'
+
+export { Cryptocurrency, Logos, Payment, Remix }
