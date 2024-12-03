b52c51b65ee3282896f52bd5d82b892202a82f38
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-12-03 22:49:05 +0300

    feat: enhance feature component with header and title styling

--- www/components/feature.vue
+++ www/components/feature.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
-import { Item } from "@blank/vue";
+import { Item, Text } from "@blank/vue";
 </script>
 
 <template>
     <Item class="feature" col="full">
+        <div class="header">
+            <Text class="title" variant="body2" tag="span">
+                Categories
+            </Text>
+        </div>
         <div class="content">
 
         </div>
@@ -14,10 +19,16 @@ import { Item } from "@blank/vue";
 .feature {
     @apply relative w-full px-12 h-full flex justify-between gap-4;
 
+    .header {
+        @apply border-b border-alpha-black-10 pb-4;
+
+        .title {
+            @apply text-primary-900 font-medium;
+        }
+    }
+
     .content {
         @apply h-full justify-center flex flex-col gap-4;
-
-
     }
 }
 </style>
